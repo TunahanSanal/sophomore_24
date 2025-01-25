df902731eae693c3d838f53030c1994246022c6c
tunahansnl <tunahansanal19@gmail.com>
2025-01-26 02:15:26 +0300

    basic application

---
+++ Switch_case.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+
+main(){
+	
+	int t;
+	
+	printf("Which month with from 1 to 12:\t");
+	scanf("%d",&t);
+	
+	switch(t){
+		
+		case 1:
+			printf("JANUARY HAS 31 DAYS");
+			break;
+		case 2:
+				printf("FEBRUARY HAS 28 DAYS, BUT SOME YEAR IT'S 29 DAYS'");
+			break;
+		case 3:
+				printf("MARCH HAS 31 DAYS");
+			break;
+		case 4:
+				printf("APRIL HAS 30 DAYS");
+			break;
+		case 5:
+				printf("MAY HAS 31 DAYS");
+			break;
+		case 6:
+				printf("JUNE HAS 30 DAYS");
+			break;
+		case 7:
+				printf("JULY HAS 31 DAYS");
+			break;
+		case 8:
+				printf("AUGUST HAS 31 DAYS");
+			break;
+		case 9:
+				printf("SEPTEMBER HAS 30 DAYS");
+			break;
+		case 10:
+				printf("OCTOBER HAS 31 DAYS");
+			break;
+		case 11:
+				printf("NOVEMBER HAS 30 DAYS");
+			break;
+		case 12:
+				printf("DECEMBER HAS 31 DAYS");
+			break;
+		default:
+				printf("Please enter from 1 to 12!");
+			break;
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
